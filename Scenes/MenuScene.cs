c0d4a720a83dffcec8aeadf38c6eb4e0f2267857
Zachary Pruessner <zachpruessner@gmail.com>
2023-07-20 16:09:16 -0500

    added background image to menu

--- Scenes/MenuScene.cs
+++ Scenes/MenuScene.cs
@@ -8,20 +8,20 @@ namespace AIAscendancy.Scenes;
 internal class MenuScene : Component
 {
     private Texture2D campaignButton;
-    private Vector2 buttonPos1 = new Vector2 (50, Data.ScreenHeight / 5);
+    private Vector2 buttonPos1 = new Vector2 (50, (Data.ScreenHeight / 5) + 25);
     private Texture2D settingsButton;
-    private Vector2 buttonPos2 = new Vector2 (50, Data.ScreenHeight * 2 / 5);
+    private Vector2 buttonPos2 = new Vector2 (50, (Data.ScreenHeight * 2 / 5) + 25);
     private Texture2D quitButton;
-    private Vector2 buttonPos3 = new Vector2 (50, Data.ScreenHeight * 3 / 5);
-    private Texture2D shuttle;
-    private Vector2 shuttlePos = new Vector2 (Data.ScreenWidth / 2, Data.ScreenHeight  / 2);
+    private Vector2 buttonPos3 = new Vector2 (50, (Data.ScreenHeight * 3 / 5) + 25);
+    private Texture2D menuBackground;
+    private Rectangle menuBackgroundRect = new Rectangle(0, 0, Data.ScreenWidth, Data.ScreenHeight);
 
     internal override void LoadContent(ContentManager Content)
     {
+        menuBackground = Content.Load<Texture2D>("menuBackground");
         campaignButton = Content.Load<Texture2D>("campaignButton");
         settingsButton = Content.Load<Texture2D>("settingsButton");
         quitButton = Content.Load<Texture2D>("quitButton");
-        shuttle = Content.Load<Texture2D>("shuttle");
     }
 
     internal override void Update(GameTime gameTime)
@@ -30,9 +30,9 @@ internal class MenuScene : Component
 
     internal override void Draw(SpriteBatch spriteBatch)
     {
+        spriteBatch.Draw(menuBackground, menuBackgroundRect, Color.White);
         spriteBatch.Draw(campaignButton, buttonPos1,  Color.White);
         spriteBatch.Draw(settingsButton, buttonPos2,  Color.White);
         spriteBatch.Draw(quitButton, buttonPos3,  Color.White);
-        spriteBatch.Draw(shuttle, shuttlePos, Color.White);
     }
 }
